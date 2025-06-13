8ad8ae5ce95be3251ab0b1ffad38a1bf713a6e4a
wanderson-xyz <124519860+wanderson-xyz@users.noreply.github.com>
2025-06-13 11:41:40 -0300

    Add files via upload

---
+++ 1059.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+ 
+int main() {
+ 
+    
+
+    int a = 2;
+
+    for(int i = 1; i<=50; i++){
+        int numPar = i * a;
+        cout <<numPar << endl;
+    }
+ 
+    return 0;
+}
